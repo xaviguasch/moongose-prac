cf6e3e23392b3098f582bb8752d95585a4474d3e
xaviguasch <xaviguasch@gmail.com>
2019-01-04 11:36:41 +0100

    playing with mongoose

--- cats.js
+++ cats.js
@@ -8,8 +8,45 @@ const catSchema = new mongoose.Schema({
 })
 
 const Cat = mongoose.model('Cat', catSchema)
+
+
 // adding a new cat to the DB
 
+// const george = new Cat({
+//     name: 'Mrs. Norris',
+//     age: 7,
+//     temperament: 'Evil'
+// })
 
+// george.save(function(err, cat) {
+//     if (err) {
+//         console.log('SOMETHING WENT WRONG!')
+//     } else {
+//         console.log('We just saved a cat to the DB:')
+//         console.log(cat)
+//     }
+// })
+
+Cat.create({
+    name: 'Snow White',
+    age: 15,
+    temperament: 'Bland'
+}, function (err, cat) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(cat);
+    }
+})
 
 // retrieve all cats from the DB and console.log each one
+
+Cat.find({}, function(err, cats) {
+    if(err) {
+        console.log('Oh, no, error!');
+        console.log(err);
+    } else {
+        console.log('all the cats:');
+        console.log(cats);        
+    }
+})
